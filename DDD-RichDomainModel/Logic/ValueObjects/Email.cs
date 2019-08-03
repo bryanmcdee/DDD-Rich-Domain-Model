96dd32cfde075bc10ba6ef0059857a50fb28f339
Bryan McDonald <bryanmcd.gt@gmail.com>
2019-08-03 16:58:07 -0400

    Added an email value object.

---
+++ DDD-RichDomainModel/Logic/ValueObjects/Email.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Logic.ValueObjects
+{
+    public class Email : ValueObject<Email>
+    {
+        public string Value { get; }
+
+        public Email(string value)
+        {
+            Value = value;
+        }
+        protected override bool EqualsCore(Email other)
+        {
+            return Value.Equals(other.Value, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
