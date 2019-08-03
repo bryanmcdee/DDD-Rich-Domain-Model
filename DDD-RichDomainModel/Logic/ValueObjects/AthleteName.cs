c38f687916e177948a2ed797916ebf9c524f91ec
Bryan McDonald <bryanmcd.gt@gmail.com>
2019-08-03 16:57:12 -0400

    Added an athlete name value object.

---
+++ DDD-RichDomainModel/Logic/ValueObjects/AthleteName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Logic.ValueObjects
+{
+    public class AthleteName : ValueObject<AthleteName>
+    {
+        public string Value { get; }
+
+        public AthleteName(string value)
+        {
+            Value = value;
+        }
+        protected override bool EqualsCore(AthleteName other)
+        {
+            return Value.Equals(other.Value, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
