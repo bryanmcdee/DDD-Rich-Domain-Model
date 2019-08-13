169047481e3cfe92e205fa3c8e675aec8295ad77
Bryan McDonald <bryanmcd.gt@gmail.com>
2019-08-13 12:53:36 -0400

    Added initial stubs of the repository interfaces. Moved SessionFactory and the UnitOfWork. Corrected a build error.

---
+++ DDD-RichDomainModel-CleanArchitecture/Common/Repositories/IAthleteRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Repositories
+{
+    public interface IAthleteRepository
+    {       
+    }
+}
---
+++ DDD-RichDomainModel-CleanArchitecture/Common/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+    }
+}
